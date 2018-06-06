3db8b8b9be7f3ddba300379a0bd922c17074a9f8
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-06-06 21:47:04 +0700

    added empty message on snippet list

--- browser/render/components/snippet-list/index.jsx
+++ browser/render/components/snippet-list/index.jsx
@@ -10,9 +10,39 @@ export default class SnippetList extends React.Component {
   handleCreateSnippetClick () {
     eventEmitter.emit('modal:open', 'createSnippetModal')
   }
-  render () {
+
+  renderSnippetList () {
     const { snippets } = this.props.store
     const { config } = this.props
+    return (
+      snippets.length > 0 &&
+      <div className='snippets'>
+        <ul>
+          {
+            snippets.map(snippet => (
+              <li key={snippet.key}>
+                <SnippetItem
+                  snippet={snippet}
+                  config={config}
+                  store={store}/>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+
+  renderEmptyMessage () {
+    return (
+      <h1 align='center' style={{ marginTop: '200px', color: 'rgba(255, 255, 255, 0.3)'}}>
+        Create new snippet using Ctrl + N
+      </h1>
+    )
+  }
+
+  render () {
+    const { snippets } = this.props.store
     return (
       <div className='snippet-list'>
         <div className='search-bar'>
@@ -28,20 +58,11 @@ export default class SnippetList extends React.Component {
           </button>
         </div>
 
-        <div className='snippets'>
-          <ul>
-            {
-              snippets.map(snippet => (
-                <li key={snippet.key}>
-                  <SnippetItem
-                    snippet={snippet}
-                    config={config}
-                    store={store}/>
-                </li>
-              ))
-            }
-          </ul>
-        </div>
+        {
+          snippets.length > 0
+            ? this.renderSnippetList()
+            : this.renderEmptyMessage()
+        }
       </div>
     )
   }
