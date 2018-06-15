e6539538484bde1f73745e2c30aa401f71791e91
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-06-15 16:07:15 +0700

    finished snippet type picker

--- browser/render/modals/modal-list.jsx
+++ browser/render/modals/modal-list.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CreateSnippetModal from './create-snippet'
+import CreateMultiFilesSnippetModal from './create-multi-files-snippet'
 import PickSnippetType from './pick-snippet-type'
 import SettingModal from './setting'
 
@@ -7,9 +8,18 @@ export default class ModalList extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <CreateSnippetModal config={this.props.config} store={this.props.store}/>
-        <SettingModal config={this.props.config} store={this.props.store} />
-        <PickSnippetType config={this.props.config} store={this.props.store} />
+        <CreateSnippetModal
+          config={this.props.config}
+          store={this.props.store}/>
+        <CreateMultiFilesSnippetModal
+          config={this.props.config}
+          store={this.props.store} />
+        <SettingModal
+          config={this.props.config}
+          store={this.props.store} />
+        <PickSnippetType
+          config={this.props.config}
+          store={this.props.store} />
       </React.Fragment>
     )
   }
--- browser/render/modals/pick-snippet-type/index.jsx
+++ browser/render/modals/pick-snippet-type/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ModalSkeleton from '../modal-skeleton'
 import i18n from 'render/lib/i18n'
+import eventEmitter from 'lib/event-emitter'
 import './pick-snippet-type'
 
 export default class PickSnippetType extends React.Component {
@@ -11,24 +12,48 @@ export default class PickSnippetType extends React.Component {
     }
   }
 
+  handleSingleFileSnippetClick () {
+    eventEmitter.emit('modal:close', this.state.name)
+    eventEmitter.emit('modal:open', 'createSnippetModal')
+  }
+
+  handleMultiFileSnippetClick () {
+    eventEmitter.emit('modal:close', this.state.name)
+    eventEmitter.emit('modal:open', 'createMultiFilesSnippetModal')
+  }
+
   render () {
     i18n.setLocale(this.props.config.ui.language)
     return (
-      <ModalSkeleton name='pickSnippetTypeModal' width='600px'>
+      <ModalSkeleton name={this.state.name} width='600px'>
         <div className='pick-snippet-type'>
-          <h2 className='m-b-30'>Create new snippet</h2>
-          <div className='button'>
-            <h3>Single-file snippet</h3>
+          <h2 className='m-b-30'>{i18n.__('Create new snippet')}</h2>
+          <div
+            className='button'
+            onClick={this.handleSingleFileSnippetClick.bind(this)}>
+            <h3>{i18n.__('Single-file snippet')}</h3>
             <p>
-              This is the type of snippet where you will only store 1 piece of
-              code. This type of snippet is suitable for storing small functions
+              {
+                i18n.__(
+                  'This is the type of snippet where you will only store 1' +
+                  ' piece of code. ' +
+                  'This type of snippet is suitable for storing small functions'
+                )
+              }
             </p>
           </div>
-          <div className='button'>
-            <h3>Multi-file snippet</h3>
+          <div
+            className='button'
+            onClick={this.handleMultiFileSnippetClick.bind(this)}>
+            <h3>{i18n.__('Multi-file snippet')}</h3>
             <p>
-              This type of snippet can groups multiple files into 1 snippet and
-              suitable for storing simple code snippet with more than 1 file
+              {
+                i18n.__(
+                  'This type of snippet can groups multiple files into 1' +
+                  ' snippet and suitable for storing simple code snippet with' +
+                  ' more than 1 file'
+                )
+              }
             </p>
           </div>
         </div>
