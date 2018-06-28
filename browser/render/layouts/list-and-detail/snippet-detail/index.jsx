602649794ec03b07bd46e256b68ebc115838bcf7
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-06-28 19:25:06 +0700

    fixed snippet detail pushed down

--- browser/render/layouts/list-and-detail/snippet-detail/index.jsx
+++ browser/render/layouts/list-and-detail/snippet-detail/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './snippet-detail'
 
 export default class SnippetDetail extends React.Component {
   render () {
-    return <h1>Hello</h1>
+    return (
+      <div className="snippet-detail">
+        <h1>Hello</h1>
+      </div>
+    )
   }
 }
