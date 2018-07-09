231f9a9d0f6e950d3d3759be4543fd52a22e2cfb
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-07-09 20:55:26 +0700

    fixed tray icon on windows

--- app/app-tray.js
+++ app/app-tray.js
@@ -1,7 +1,12 @@
-const { Menu, Tray } = require('electron')
+const { Menu, Tray, nativeImage } = require('electron')
+const path = require('path')
 
 function createTray (app, mainWindow) {
-  const tray = new Tray('resources/icon/icon512.png')
+  const tray = new Tray(
+    nativeImage.createFromPath(
+      path.join(__dirname, '..', 'resources', 'icon', 'icon512.png')
+    )
+  )
 
   const contextMenu = Menu.buildFromTemplate([
     {
