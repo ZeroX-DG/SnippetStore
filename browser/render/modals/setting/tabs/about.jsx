7882eb316f24d4e80ea19556ad2e9a3a2f7d272f
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2019-01-24 11:03:30 +0700

    fixed eslint error

--- browser/render/modals/setting/tabs/about.jsx
+++ browser/render/modals/setting/tabs/about.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import logo from "resources/icon/icon512.png";
-import { shell, remote } from "electron";
-import i18n from "render/lib/i18n";
-import Switch from "render/components/switch";
-import ConfigManager from "lib/config-manager";
-import { pageView } from "lib/analytics";
-import donateButton from "resources/image/donate.png";
+import React from 'react'
+import logo from 'resources/icon/icon512.png'
+import { shell, remote } from 'electron'
+import i18n from 'render/lib/i18n'
+import Switch from 'render/components/switch'
+import ConfigManager from 'lib/config-manager'
+import { pageView } from 'lib/analytics'
+import donateButton from 'resources/image/donate.png'
 
-const { app } = remote;
+const { app } = remote
 
 export default class About extends React.Component {
-  componentDidMount() {
-    const links = document.querySelectorAll("a");
+  componentDidMount () {
+    const links = document.querySelectorAll('a')
     links.forEach(link => {
-      link.addEventListener("click", e => {
-        e.preventDefault();
-        shell.openExternal(link.href);
-      });
-    });
-    pageView("/setting/about");
+      link.addEventListener('click', e => {
+        e.preventDefault()
+        shell.openExternal(link.href)
+      })
+    })
+    pageView('/setting/about')
   }
 
-  onAnalyticsChange(checked) {
-    ConfigManager.set({ allowAnalytics: checked });
+  onAnalyticsChange (checked) {
+    ConfigManager.set({ allowAnalytics: checked })
   }
 
-  render() {
-    const { config } = this.props;
+  render () {
+    const { config } = this.props
     return (
       <div className="about-tab">
-        <h1 className="tab-title">{i18n.__("About")}</h1>
+        <h1 className="tab-title">{i18n.__('About')}</h1>
         <div className="content">
           <div className="inline">
             <img src={logo} width="200px" />
@@ -37,7 +37,7 @@ export default class About extends React.Component {
               <h2>SnippetStore</h2>
               <p>Version: {app.getVersion()}</p>
               <p className="m-t-20">
-                {i18n.__("SnippetStore is a snippet management app developers")}
+                {i18n.__('SnippetStore is a snippet management app developers')}
               </p>
             </div>
           </div>
@@ -61,10 +61,10 @@ export default class About extends React.Component {
           <h2 className="m-t-50">Analytics</h2>
           <p>
             {i18n.__(
-              "SnippetStore collects anonymous data for only 1 reason" +
-                "-improve user experience. We will never collects your private " +
-                "information such as your code snippets. If you wish to know " +
-                "how it work, here is its source code:"
+              'SnippetStore collects anonymous data for only 1 reason' +
+                '-improve user experience. We will never collects your private ' +
+                'information such as your code snippets. If you wish to know ' +
+                'how it work, here is its source code:'
             )}
             &nbsp;
             <a href="https://github.com/ZeroX-DG/SnippetStore">github</a>
@@ -78,6 +78,6 @@ export default class About extends React.Component {
           </p>
         </div>
       </div>
-    );
+    )
   }
 }
