36da556362b47d29fb9b9b795fc69d98f8414c13
ZeroX-DG <viethungax@gmail.com>
2020-09-02 12:08:52 +1200

    updated dependencies

--- app/app-main.js
+++ app/app-main.js
@@ -1,53 +1,51 @@
-const { app, Menu, ipcMain } = require('electron')
+const { app, Menu, ipcMain } = require("electron");
 
-let mainWindow = null
+let mainWindow = null;
 
-const isSecondInstance = app.makeSingleInstance(function (
-  commandLine,
-  workingDirectory
-) {
-  if (mainWindow) {
-    if (process.platform === 'win32') {
-      mainWindow.minimize()
-      mainWindow.restore()
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on("second-instance", (event, commandLine, workingDirectory) => {
+    if (mainWindow) {
+      if (process.platform === "win32") {
+        mainWindow.minimize();
+        mainWindow.restore();
+      }
+      mainWindow.show();
+      mainWindow.focus();
     }
-    mainWindow.show()
-    mainWindow.focus()
-  }
-  return true
-})
+  });
 
-if (isSecondInstance) {
-  app.exit()
+  app.on("ready", () => {
+    mainWindow = require("./app-window")(app);
+    require("./app-tray")(app, mainWindow);
+    ipcMain.on("bringToFront", () => {
+      if (process.platform === "win32") {
+        mainWindow.minimize();
+        mainWindow.restore();
+      }
+      mainWindow.show();
+      mainWindow.focus();
+    });
+    const template = require("./app-menu")(app, mainWindow);
+    const menu = Menu.buildFromTemplate(template);
+    switch (process.platform) {
+      case "darwin":
+        Menu.setApplicationMenu(menu);
+        break;
+      case "win32":
+        mainWindow.setMenu(menu);
+        break;
+      case "linux":
+        Menu.setApplicationMenu(menu);
+        mainWindow.setMenu(menu);
+        break;
+    }
+  });
+
+  app.on("before-quit", () => {
+    mainWindow.shouldQuit();
+  });
 }
-
-app.on('ready', () => {
-  mainWindow = require('./app-window')(app)
-  require('./app-tray')(app, mainWindow)
-  ipcMain.on('bringToFront', () => {
-    if (process.platform === 'win32') {
-      mainWindow.minimize()
-      mainWindow.restore()
-    }
-    mainWindow.show()
-    mainWindow.focus()
-  })
-  const template = require('./app-menu')(app, mainWindow)
-  const menu = Menu.buildFromTemplate(template)
-  switch (process.platform) {
-    case 'darwin':
-      Menu.setApplicationMenu(menu)
-      break
-    case 'win32':
-      mainWindow.setMenu(menu)
-      break
-    case 'linux':
-      Menu.setApplicationMenu(menu)
-      mainWindow.setMenu(menu)
-      break
-  }
-})
-
-app.on('before-quit', () => {
-  mainWindow.shouldQuit()
-})
