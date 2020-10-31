9503eac7641ea69851668932a206d33a516fa1ae
ZeroX-DG <viethungax@gmail.com>
2020-10-31 15:31:24 +1300

    added italian language

--- browser/render/lib/languages.js
+++ browser/render/lib/languages.js
@@ -1,18 +1,20 @@
 const languages = [
-  { name: 'English', code: 'en' },
-  { name: 'Tiếng Việt', code: 'vi' },
+  { name: "English", code: "en" },
+  { name: "Tiếng Việt", code: "vi" },
   // thanks @zbahadir for Turkish translation
-  { name: 'Turkish', code: 'tr' }
-]
+  { name: "Turkish", code: "tr" },
+  // thanks nickyg.pf for Italian translation
+  { name: "Italian", code: "it" }
+];
 
 module.exports = {
-  getLanguageCodes () {
-    return languages.reduce(function (codes, lang) {
-      codes.push(lang.code)
-      return codes
-    }, [])
+  getLanguageCodes() {
+    return languages.reduce(function(codes, lang) {
+      codes.push(lang.code);
+      return codes;
+    }, []);
   },
-  getLanguages () {
-    return languages
+  getLanguages() {
+    return languages;
   }
-}
+};
